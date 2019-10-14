e0d6dd76553a8cb83a9300653b26750f88a493f1
glanderson42 <laveau.anderson42@protonmail.com>
2019-10-14 11:17:19 +0200

    remove matrix

--- lesson6/matrix/matrix.h
+++
@@ -1,8 +0,0 @@
-#ifndef MATRIX_H
-#define MATRIX_H
-
-void matrix_sum(int** n, int** m, int row, int col);
-void matrix_scalar_mult(int** n, int row, int col, int scalar);
-void matrix_mult(int** n, int** m, int row, int col);
-
-#endif
